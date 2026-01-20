429058eea5c626d71560c0e5ef1620028b7e2080
Sam <doansamquoc@gmail.com>
2026-01-20 17:18:34 +0700

    Initial project

---
+++ src/main/java/com/sam/miniecommerceapi/MiniEcommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.sam.miniecommerceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniEcommerceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniEcommerceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sam/miniecommerceapi/MiniEcommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.miniecommerceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniEcommerceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
