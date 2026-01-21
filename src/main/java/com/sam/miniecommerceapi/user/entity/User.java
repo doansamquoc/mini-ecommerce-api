3bbb118caa62c4228432fcf75791727da4b1815b
Sam <doansamquoc@gmail.com>
2026-01-21 09:18:48 +0700

    chore: removed libs unused

--- src/main/java/com/sam/miniecommerceapi/user/entity/User.java
+++ src/main/java/com/sam/miniecommerceapi/user/entity/User.java
@@ -5,15 +5,6 @@ import com.sam.miniecommerceapi.common.enums.Role;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
-import org.jspecify.annotations.NullUnmarked;
-import org.jspecify.annotations.Nullable;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 import java.util.Set;
 
 @Setter
