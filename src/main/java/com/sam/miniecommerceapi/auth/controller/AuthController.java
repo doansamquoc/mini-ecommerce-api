97695f5831fc38365d2585c9c5ca320318bed8ee
Sam <doansamquoc@gmail.com>
2026-03-14 06:25:40 +0700

    feature(token): add refresh method to get new access token

--- src/main/java/com/sam/miniecommerceapi/auth/controller/AuthController.java
+++ src/main/java/com/sam/miniecommerceapi/auth/controller/AuthController.java
@@ -1,6 +1,6 @@
 package com.sam.miniecommerceapi.auth.controller;
 
-import com.sam.miniecommerceapi.auth.dto.internal.LoginResult;
+import com.sam.miniecommerceapi.auth.dto.internal.TokenDTO;
 import com.sam.miniecommerceapi.auth.dto.request.UserCreationRequest;
 import com.sam.miniecommerceapi.auth.dto.request.ForgotPasswordRequest;
 import com.sam.miniecommerceapi.auth.dto.request.LoginRequest;
@@ -9,12 +9,14 @@ import com.sam.miniecommerceapi.auth.dto.response.AuthResponse;
 import com.sam.miniecommerceapi.auth.service.AuthenticationService;
 import com.sam.miniecommerceapi.auth.service.IdentityService;
 import com.sam.miniecommerceapi.auth.service.PasswordService;
+import com.sam.miniecommerceapi.auth.service.TokenManagementService;
 import com.sam.miniecommerceapi.common.annotation.ClientIp;
 import com.sam.miniecommerceapi.common.annotation.UserAgent;
 import com.sam.miniecommerceapi.common.api.SuccessApi;
 import com.sam.miniecommerceapi.common.api.factory.ApiFactory;
 import com.sam.miniecommerceapi.common.service.CookieService;
 import com.sam.miniecommerceapi.user.dto.response.UserResponse;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -32,6 +34,7 @@ public class AuthController {
     PasswordService passwordService;
     IdentityService identityService;
     AuthenticationService authenticationService;
+    TokenManagementService tokenManagementService;
 
     @PostMapping("/login")
     ResponseEntity<SuccessApi<AuthResponse>> authenticate(
@@ -39,9 +42,9 @@ public class AuthController {
             @ClientIp String ip,
             @UserAgent String agent
     ) {
-        LoginResult result = authenticationService.login(loginRequest, ip, agent);
-        ResponseCookie cookie = cookieService.createRefreshToken(result.getRefreshToken());
-        AuthResponse authResponse = new AuthResponse(result.getAccessToken());
+        TokenDTO token = authenticationService.login(loginRequest, ip, agent);
+        ResponseCookie cookie = cookieService.createRefreshToken(token.getRefreshToken());
+        AuthResponse authResponse = new AuthResponse(token.getAccessToken());
 
         return ApiFactory.success(cookie.toString(), authResponse, "Login successfully!");
     }
@@ -76,5 +79,18 @@ public class AuthController {
     ResponseEntity<SuccessApi<String>> verifyPasswordResetToken(@RequestParam("token") String token) {
         passwordService.validateResetToken(token);
         return ApiFactory.success("Verify token successfully!");
+    }
+
+    @GetMapping("/refresh")
+    ResponseEntity<SuccessApi<AuthResponse>> refresh(
+            @CookieValue(name = "refresh-token", required = false) String refreshTokenStr,
+            @ClientIp String ip,
+            @UserAgent String agent
+    ) {
+        TokenDTO token = tokenManagementService.refreshAccessToken(refreshTokenStr, ip, agent);
+        ResponseCookie cookie = cookieService.createRefreshToken(token.getRefreshToken());
+        AuthResponse response = new AuthResponse(token.getAccessToken());
+
+        return ApiFactory.success(cookie.toString(), response, "Refresh access token successfully.");
     }
 }
