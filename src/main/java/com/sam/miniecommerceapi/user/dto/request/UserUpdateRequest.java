a0595076c3de0fba9615422ff7dd6b206b17c8a5
Sam <doansamquoc@gmail.com>
2026-03-14 14:29:27 +0700

    refactor(user): removed @NotBlank annotation, because fields that are not update will be blank

--- src/main/java/com/sam/miniecommerceapi/user/dto/request/UserUpdateRequest.java
+++ src/main/java/com/sam/miniecommerceapi/user/dto/request/UserUpdateRequest.java
@@ -14,7 +14,6 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserUpdateRequest {
 
-    @NotBlank(message = "USER_USERNAME_CANNOT_BLANK")
     @Size(min = 2, message = "USER_USERNAME_TOO_SHORT")
     @Size(max = 16, message = "USER_USERNAME_TOO_LONG")
     String username;
@@ -24,12 +23,10 @@ public class UserUpdateRequest {
 
     String phone;
 
-    @NotBlank(message = "USER_PASSWORD_CANNOT_BLANK")
     @Size(min = 6, message = "USER_PASSWORD_TOO_SHORT")
     @Size(max = 56, message = "USER_PASSWORD_TOO_LONG")
     String password;
 
-    @NotBlank(message = "USER_DISPLAY_NAME_CANNOT_BLANK")
     @Size(min = 2, message = "USER_DISPLAY_NAME_TOO_SHORT")
     @Size(max = 56, message = "USER_DISPLAY_NAME_TOO_LONG")
     String displayName;
