30f0b9b9c1a2e74416c2c2b70bcb09fb3c6bab13
Sam <doansamquoc@gmail.com>
2026-04-10 03:15:18 +0700

    refactor: remove CookieService to use CookieUtils instead

--- src/main/java/com/sam/miniecommerceapi/common/util/CookieUtils.java
+++ src/main/java/com/sam/miniecommerceapi/common/util/CookieUtils.java
@@ -47,6 +47,6 @@ public class CookieUtils {
 	}
 
 	public static <T> T deserialize(Cookie cookie, Class<T> cls) {
-		return cls.cast(SerializationUtils.clone(Base64.getUrlDecoder().decode(cookie.getValue())));
+		return cls.cast(SerializationUtils.deserialize(Base64.getUrlDecoder().decode(cookie.getValue())));
 	}
 }
